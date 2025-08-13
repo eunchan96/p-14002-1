559ec844769ccfaed104cefb4d28aaa7720e4bc3
eunchan96 <taegwon02@gmail.com>
2025-08-13 11:40:26 +0900

    PostCommentDto

---
+++ backend/src/main/java/com/back/domain/post/postComment/dto/PostCommentDto.kt
@@ -0,0 +1,26 @@
+package com.back.domain.post.postComment.dto
+
+import com.back.domain.post.postComment.entity.PostComment
+import lombok.Getter
+import java.time.LocalDateTime
+
+@Getter
+class PostCommentDto(postComment: PostComment) {
+    val id: Int
+    val createDate: LocalDateTime
+    val modifyDate: LocalDateTime
+    val authorId: Int
+    val authorName: String
+    val postId: Int
+    val content: String
+
+    init {
+        id = postComment.id
+        createDate = postComment.createDate
+        modifyDate = postComment.modifyDate
+        authorId = postComment.author.id
+        authorName = postComment.author.name
+        postId = postComment.post.id
+        content = postComment.content
+    }
+}
