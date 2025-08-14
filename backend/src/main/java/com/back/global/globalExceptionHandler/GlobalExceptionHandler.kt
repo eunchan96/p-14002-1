43d5a6af049881c05cdc09bc1718f37fc1264c6c
eunchan96 <taegwon02@gmail.com>
2025-08-14 10:47:32 +0900

    GlobalExceptionHandler 2

--- backend/src/main/java/com/back/global/globalExceptionHandler/GlobalExceptionHandler.kt
+++ backend/src/main/java/com/back/global/globalExceptionHandler/GlobalExceptionHandler.kt
@@ -2,7 +2,6 @@ package com.back.global.globalExceptionHandler
 
 import com.back.global.exception.ServiceException
 import com.back.global.rsData.RsData
-import jakarta.validation.ConstraintViolation
 import jakarta.validation.ConstraintViolationException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -18,78 +17,84 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 class GlobalExceptionHandler {
     @ExceptionHandler(NoSuchElementException::class)
     fun handle(ex: NoSuchElementException): ResponseEntity<RsData<Void>> {
-        return ResponseEntity<RsData<Void>>(
-            RsData(
-                "404-1",
-                "해당 데이터가 존재하지 않습니다."
-            ),
-            HttpStatus.NOT_FOUND
-        )
+        return ResponseEntity
+            .status(HttpStatus.NOT_FOUND)
+            .body(
+                RsData(
+                    "404-1",
+                    "해당 데이터가 존재하지 않습니다."
+                )
+            )
     }
 
 
     @ExceptionHandler(ConstraintViolationException::class)
     fun handle(ex: ConstraintViolationException): ResponseEntity<RsData<Void>> {
         val message = ex.constraintViolations
-            .map { violation: ConstraintViolation<*> ->
-                val field: String =
-                    violation.propertyPath.toString().split("\\.".toRegex(), limit = 2)[1]
-                val messageTemplateBits = violation.messageTemplate
-                    .split("\\.".toRegex()).dropLastWhile { it.isEmpty() }
-                val code = messageTemplateBits[messageTemplateBits.size - 2]
-                val _message = violation.message
-                "$field-$code-$_message"
+            .map { violation ->
+                val field = violation.propertyPath
+                    .toString()
+                    .split('.', limit = 2)[1]
+                val code = violation.messageTemplate
+                    .split('.')
+                    .dropLastWhile { it.isEmpty() }
+                    .let { it[it.size - 2] }
+                "$field-$code-${violation.message}"
             }
             .sorted()
             .joinToString("\n")
 
-        return ResponseEntity<RsData<Void>>(
-            RsData(
-                "400-1",
-                message
-            ),
-            HttpStatus.BAD_REQUEST
-        )
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(
+                RsData(
+                    "400-1",
+                    message
+                )
+            )
     }
 
     @ExceptionHandler(MethodArgumentNotValidException::class)
     fun handle(ex: MethodArgumentNotValidException): ResponseEntity<RsData<Void>> {
         val message = ex.bindingResult.allErrors
-            .filter { error -> error is FieldError }
-            .map { error -> error as FieldError }
-            .map { error -> "${error.field}-${error.code}-${error.defaultMessage}" }
+            .filter { it is FieldError }
+            .map { it as FieldError }
+            .map { "${it.field}-${it.code}-${it.defaultMessage}" }
             .sorted()
             .joinToString("\n")
 
-        return ResponseEntity<RsData<Void>>(
-            RsData(
-                "400-1",
-                message
-            ),
-            HttpStatus.BAD_REQUEST
-        )
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(
+                RsData(
+                    "400-1",
+                    message
+                )
+            )
     }
 
     @ExceptionHandler(HttpMessageNotReadableException::class)
     fun handle(ex: HttpMessageNotReadableException?): ResponseEntity<RsData<Void>> {
-        return ResponseEntity<RsData<Void>>(
-            RsData(
-                "400-1",
-                "요청 본문이 올바르지 않습니다."
-            ),
-            HttpStatus.BAD_REQUEST
-        )
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(
+                RsData(
+                    "400-1",
+                    "요청 본문이 올바르지 않습니다."
+                )
+            )
     }
 
     @ExceptionHandler(MissingRequestHeaderException::class)
     fun handle(ex: MissingRequestHeaderException): ResponseEntity<RsData<Void>> {
-        return ResponseEntity<RsData<Void>>(
-            RsData(
-                "400-1",
-                "${ex.headerName}-NotBlank-${ex.localizedMessage}"
-            ),
-            HttpStatus.BAD_REQUEST
-        )
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(
+                RsData(
+                    "400-1",
+                    "${ex.headerName}-NotBlank-${ex.localizedMessage}"
+                )
+            )
     }
 
     @ExceptionHandler(ServiceException::class)
@@ -97,12 +102,8 @@ class GlobalExceptionHandler {
     fun handle(ex: ServiceException): ResponseEntity<RsData<Void>> {
         val rsData: RsData<Void> = ex.rsData
 
-        return ResponseEntity<RsData<Void>>(
-            rsData,
-            ResponseEntity
-                .status(rsData.statusCode)
-                .build<Any>()
-                .statusCode
-        )
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(rsData)
     }
 }
