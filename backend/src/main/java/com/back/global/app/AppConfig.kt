c95bf2c01f8a62baa63be542bdf581407186787b
eunchan96 <taegwon02@gmail.com>
2025-08-14 12:05:37 +0900

    CustomConfigProperties 2

--- backend/src/main/java/com/back/global/app/AppConfig.kt
+++ backend/src/main/java/com/back/global/app/AppConfig.kt
@@ -2,6 +2,7 @@ package com.back.global.app
 
 import com.back.standard.util.Ut
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.env.Environment
@@ -9,6 +10,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 
 @Configuration
+@ConfigurationPropertiesScan
 class AppConfig (
     environment: Environment,
     objectMapper: ObjectMapper
--- backend/src/main/java/com/back/global/app/CustomConfigProperties.kt
+++ backend/src/main/java/com/back/global/app/CustomConfigProperties.kt
@@ -1,9 +1,7 @@
 package com.back.global.app
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.context.annotation.Configuration
 
-@Configuration
 @ConfigurationProperties(prefix = "custom")
 class CustomConfigProperties (
     val notProdMembers: List<NotProdMember>
