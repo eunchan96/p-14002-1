93ec53d78aad7bf07058170aa390682bbbae680b
eunchan96 <taegwon02@gmail.com>
2025-08-18 10:38:26 +0900

    base64 encode/decode

--- backend/src/main/java/com/back/global/security/CustomOAuth2LoginSuccessHandler.kt
+++ backend/src/main/java/com/back/global/security/CustomOAuth2LoginSuccessHandler.kt
@@ -2,13 +2,12 @@ package com.back.global.security
 
 import com.back.domain.member.member.service.MemberService
 import com.back.global.rq.Rq
+import com.back.standard.extensions.base64Decode
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.security.core.Authentication
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler
 import org.springframework.stereotype.Component
-import java.nio.charset.StandardCharsets
-import java.util.*
 
 @Component
 class CustomOAuth2LoginSuccessHandler(
@@ -30,7 +29,7 @@ class CustomOAuth2LoginSuccessHandler(
         val redirectUrl = request.getParameter("state")
             ?.let { encoded ->
                 runCatching {
-                    String(Base64.getDecoder().decode(encoded), StandardCharsets.UTF_8)
+                    encoded.base64Decode()
                 }.getOrNull()
             }
             ?.substringBefore("#")
