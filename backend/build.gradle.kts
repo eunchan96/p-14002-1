abd475c1b2b2159f17797770bfc82f9caad62f0c
eunchan96 <taegwon02@gmail.com>
2025-08-21 14:29:23 +0900

    ManyToOne에는 lazy 붙이기

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -69,8 +69,8 @@ kotlin {
     }
 }
 
-//allOpen {
-//    annotation("jakarta.persistence.Entity")
-//    annotation("jakarta.persistence.MappedSuperclass")
-//    annotation("jakarta.persistence.Embeddable")
-//}
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
+}
