3d67ffe5b2d5c319fcdcbb29ffc650aabbd73100
eunchan96 <taegwon02@gmail.com>
2025-09-05 10:21:36 +0900

    PostUser repository, service

---
+++ backend/src/main/kotlin/com/back/domain/post/postUser/service/PostUserService.kt
@@ -0,0 +1,12 @@
+package com.back.domain.post.postUser.service
+
+import com.back.domain.post.postUser.entity.PostUser
+import com.back.domain.post.postUser.repository.PostUserRepository
+
+class PostUserService(
+    private val postUserRepository: PostUserRepository
+) {
+    fun findByUsername(username: String): PostUser? {
+        return postUserRepository.findByUsername(username)
+    }
+}
