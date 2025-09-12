92d25e4c27f60a88b62716b421c3642b2067ede5
eunchan96 <taegwon02@gmail.com>
2025-09-12 09:18:28 +0900

    infra

---
+++ infra/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  description = "region"
+  default     = "ap-northeast-2"
+}
+
+variable "prefix" {
+  description = "Prefix for all resources"
+  default     = "team"
+}
+
+variable "app_1_domain" {
+  description = "app_1 domain"
+  default     = "api.p-14002-1.bechan.site"
+}
